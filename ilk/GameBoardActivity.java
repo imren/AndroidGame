84220d42e715c915222d4645a38ce6d062c48871
elifaydos <aydoselif@gmail.com>
2013-04-02 01:13:10 +0300

    ilk commitim

---
+++ ilk/GameBoardActivity.java
@@ -0,0 +1,20 @@
+package com.examp.androidgame_0;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GameBoardActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.game_board);
+	}
+
+	@Override
+	protected void onStart(){
+		super.onStart();
+	}
+
+}
